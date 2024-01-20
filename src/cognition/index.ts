1ff84af63e881b9f72fb6fb977cadf1e93e361af
CJ Pais <cj@cjpais.com>
2024-01-19 16:15:31 -0800

    add generic querying of the data

--- src/cognition/index.ts
+++ src/cognition/index.ts
@@ -11,3 +11,77 @@ export const summarize = async (
     "gpt-4-1106-preview"
   );
 };
+
+export type RunCompletionParams = {
+  name: string;
+  systemPrompt?: string;
+  userPrompt: string;
+  model?: "gpt-3.5-turbo-1106" | "gpt-4-1106-preview";
+};
+
+const CODE_REGEX = /```.*?\n([\s\S]*?)```/;
+
+export const runCodeCompletion = async ({
+  name,
+  systemPrompt = "You are a helpful assistant.",
+  userPrompt = "",
+  model = "gpt-4-1106-preview",
+}: RunCompletionParams): Promise<string | null> => {
+  const rawCompletion = (await generateCompletion(
+    // const rawCompletion = (await generateTogetherCompletion(
+    systemPrompt,
+    userPrompt,
+    null,
+    model
+  )) as string;
+
+  const match = rawCompletion.match(CODE_REGEX);
+  if (!match || match.length < 2) {
+    console.log(
+      `NO MATCH FOUND FOR COMPLETION: ${name}\n\nRAW COMPLETION: ${rawCompletion}`
+    );
+    return null;
+  }
+
+  console.log("CODE COMPLETION MODEL", model);
+  console.log("CODE COMPLETION SYSTEM PROMPT", systemPrompt);
+  console.log("CODE COMPLETION USER PROMPT", systemPrompt);
+
+  return match[1];
+};
+
+export const runJsonCompletion = async <T>({
+  name,
+  systemPrompt = "You are a helpful assistant.",
+  userPrompt = "",
+  model = "gpt-4-1106-preview",
+}: RunCompletionParams): Promise<T | null> => {
+  const rawCompletion = (await generateCompletion(
+    // const rawCompletion = (await generateTogetherCompletion(
+    systemPrompt,
+    userPrompt,
+    null,
+    model
+  )) as string;
+
+  let result: T;
+
+  try {
+    result = JSON.parse(rawCompletion);
+    // console.log(name, systemPrompt, userPrompt, model, rawCompletion, match[1]);
+    return result;
+  } catch {
+    const match = rawCompletion.match(CODE_REGEX);
+    // console.log(name, systemPrompt, userPrompt, model, rawCompletion, match[1]);
+    if (match && match.length > 1) {
+      result = JSON.parse(match[1]);
+      return result;
+    } else {
+      console.log(
+        `NO MATCH FOUND FOR COMPLETION: ${name}\n\nRAW COMPLETION: ${rawCompletion}`
+      );
+    }
+  }
+
+  return null;
+};
--- src/server/handlers/query/index.ts
+++ src/server/handlers/query/index.ts
@@ -2,15 +2,26 @@ import { z } from "zod";
 import Ajv, { ValidateFunction } from "ajv";
 import { generateCompletion } from "../../../cognition/openai";
 import { query } from "../../../memory/vector";
+import { metadataList, validateAuthToken } from "../..";
+import dayjs from "dayjs";
+import {
+  INFERENCE_NEEDED_SYSTEM_PROMPT,
+  INTERPRET_DATA_PROMPT_TEMPLATE,
+  KEYWORD_SYS_PROMPT,
+  NEW_CODE_SYSTEM_PROMPT,
+} from "../../../../prompts";
+import Mustache from "mustache";
+import { randomUUID } from "node:crypto";
+import { keywordSearch } from "../../../tools/keyword";
+import { execute } from "../../../tools/jsvm";
+import { runCodeCompletion, runJsonCompletion } from "../../../cognition";
 
-type PipelineFunction = (input: any) => Promise<any>;
-
-const QueryRequestSchema = z.object({
+export const QueryRequestSchema = z.object({
   query: z.string(),
   schema: z.any().optional(),
 });
 
-const QueryResponseSchema = z.object({});
+const cache = {};
 
 // todo queries:
 // what have i been doing
@@ -19,10 +30,66 @@ const QueryResponseSchema = z.object({});
 // next 10 entries (chained??)
 // give me entries related to blah sorted by time
 // what is this doc related to?
+const interpretDataCompletion = async (
+  req: z.infer<typeof QueryRequestSchema>,
+  data: any
+) => {
+  const prompt = Mustache.render(INTERPRET_DATA_PROMPT_TEMPLATE, {
+    question: req.query,
+    output: JSON.stringify(req.schema, null, 2),
+    data: JSON.stringify(data, null, 2),
+  });
+
+  return await runJsonCompletion<any>({
+    name: "interpret data",
+    userPrompt: prompt,
+  });
+};
+
+const runBasicCompletions = async (req: z.infer<typeof QueryRequestSchema>) => {
+  const inferenceCompletion = runJsonCompletion<{
+    infer: boolean;
+    explanation: string;
+  }>({
+    name: "is inference necessary?",
+    systemPrompt: INFERENCE_NEEDED_SYSTEM_PROMPT,
+    userPrompt: `Question: ${req.query}\nOutput Format: ${JSON.stringify(
+      req.schema,
+      null,
+      2
+    )} `,
+  });
+
+  // TODO we dont need to parse the JSON this will cause error as is.
+  const codeCompletion = runCodeCompletion({
+    name: "generate code",
+    systemPrompt: NEW_CODE_SYSTEM_PROMPT,
+    userPrompt: `Question: ${req.query}`,
+  });
+
+  const keywordCompletion = runJsonCompletion<string[]>({
+    name: "generate keywords",
+    systemPrompt: KEYWORD_SYS_PROMPT,
+    userPrompt: `Question: ${req.query}`,
+    model: "gpt-3.5-turbo-1106",
+  });
+
+  const [inferenceNeeded, code, keywords] = await Promise.all([
+    inferenceCompletion,
+    codeCompletion,
+    keywordCompletion,
+  ]);
+
+  return { inferenceNeeded, code, keywords };
+};
 
 export const handleQueryRequest = async (request: Request) => {
   if (request.method !== "POST") {
     return new Response("Method not allowed", { status: 405 });
+  }
+
+  if (!validateAuthToken(request)) {
+    return new Response("Unauthorized", { status: 401 });
   }
 
   const ajv = new Ajv();
@@ -30,27 +97,69 @@ export const handleQueryRequest = async (request: Request) => {
   let response;
 
   try {
-    const data = await request.json();
-    const { query: q, schema } = QueryRequestSchema.parse(data);
+    const reqData = await request.json();
+    const queryRequest = QueryRequestSchema.parse(reqData);
 
-    // if there is a json schema validate it
-    // if (schema) {
-    //   if (schema instanceof Object) {
-    //     ajvSchema = ajv.compile(schema);
-    //   } else {
-    //     ajvSchema = ajv.compile(JSON.parse(schema));
-    //   }
-    // }
-    const docs = await query(q, 10);
+    const metadata = metadataList.map((m: any) => ({
+      created: m.created,
+      date: dayjs(m.created * 1000).format("MMM D, YYYY - h:mma"),
+      hash: m.hash,
+      title: m.title,
+      summary: m.summary,
+      text: m.audio ? m.audio.transcript : "",
+      embedding: m.embedding,
+    }));
 
-    const response = await generateCompletion(
-      q,
-      docs.documents[0].join("\n\n")
+    const uuid = randomUUID();
+    console.log("--------------uuid---------------", uuid);
+
+    // get the basic completions needed to execute code and generate keywords
+    const { inferenceNeeded, code, keywords } = await runBasicCompletions(
+      queryRequest
     );
 
-    return new Response(JSON.stringify({ response, docs: docs.documents[0] }), {
-      status: 200,
-    });
+    console.log("inference needed", inferenceNeeded);
+    console.log("code", code);
+    console.log("keywords", keywords);
+
+    if (!code) return new Response("Error generating code", { status: 500 });
+    if (!keywords)
+      return new Response("Error generating keywords", { status: 500 });
+    if (!inferenceNeeded)
+      return new Response("Error generating inference", { status: 500 });
+
+    let data;
+
+    // // execute the code
+    const codeData = execute(code, metadata);
+
+    // // perform keyword search
+    const keywordData = await keywordSearch(keywords, codeData);
+
+    if (inferenceNeeded.infer) {
+      data = await interpretDataCompletion(queryRequest, keywordData);
+    } else {
+      data = keywordData;
+    }
+    // return new Response(JSON.stringify({ inferenceNeeded, keywords, code }), {
+    //   status: 200,
+    // });
+
+    return new Response(
+      JSON.stringify({
+        data,
+        // generationDetails: {
+        //   inferenceNeeded,
+        //   code,
+        //   keywords,
+        //   codeData,
+        //   keywordData,
+        // },
+      }),
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.log(error);
     return new Response("Bad request", { status: 400 });
