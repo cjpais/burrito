1f594eb92d36fe330fd1bbb325b9cede4dbe51d6
CJ Pais <cj@cjpais.com>
2024-10-29 12:22:18 -0700

    next page button at top

--- src/server/ui/pages/index.tsx
+++ src/server/ui/pages/index.tsx
@@ -4,6 +4,27 @@ import Memory from "../components/memory";
 const Index = ({ metadata, page }: { metadata: any[]; page: number }) => {
   return (
     <Layout title={process.env.BRAIN_NAME!}>
+      <div
+        className="mono"
+        style={{
+          display: "flex",
+          gap: "1rem",
+          paddingTop: "2rem",
+          paddingBottom: "2rem",
+          fontSize: ".85rem",
+        }}
+      >
+        {page > 0 && (
+          <a href={`/?p=${page - 1}`} className="link">
+            previous
+          </a>
+        )}
+        {metadata.length > 0 && (
+          <a href={`/?p=${page + 1}`} className="link">
+            next
+          </a>
+        )}
+      </div>
       <div className="memory-holder">
         {metadata.map((m, id) => (
           <Memory key={id} m={m} />
